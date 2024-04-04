e4177cd3b1afec1820f62d6d101b23e82bc3b0ed
isaacgdias <isaacbogui98@gmail.com>
2024-04-04 17:24:32 -0300

    estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginLayoutComponent } from './components/login-layout/login-layout.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginLayoutComponent
+    LoginLayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+ 
+})
+export class LoginComponent {
+
+}
