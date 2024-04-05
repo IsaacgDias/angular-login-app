af984e731b2061720596f582438a6c7bd0bd40ea
isaacgdias <isaacbogui98@gmail.com>
2024-04-04 21:43:03 -0300

    css login-layout

--- src/app/components/login-layout/login-layout.component.ts
+++ src/app/components/login-layout/login-layout.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login-layout',
   templateUrl: './login-layout.component.html',
-  styleUrls: ['./login-layout.component.css']
+  styleUrls: ['./login-layout.component.scss']
 })
 export class LoginLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() btn1: string = "";
+  @Input() btn2: string = "";
 }
